f58190adfc79aa49b17201ddb3dcc5f4470664ba
mohammedshoaib2 <thismohdshoaib@gmail.com>
2024-11-17 00:52:51 +0530

    variables completed

---
+++ 01_javascript_basics/01_variables.js
@@ -0,0 +1,6 @@
+const UID = 522566266;
+let user_email = "thismohdshoaib@gmail.com"; //let is scope specific
+var user_name = "shoaib5478"; //should avoid
+age = 23; //bad practice
+
+console.table([UID, user_email, user_name, age]);
