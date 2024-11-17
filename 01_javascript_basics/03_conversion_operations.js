24fbe45ba99fddf7a46502e11032e5c5b89c6183
mohammedshoaib2 <thismohdshoaib@gmail.com>
2024-11-17 14:56:34 +0530

    data conversions in JS

---
+++ 01_javascript_basics/03_conversion_operations.js
@@ -0,0 +1,23 @@
+let number = 100; //number
+let name = "example"; //NaN
+let marks = "98"; //number
+let is_logged_in = true; //1
+let is_eligible = undefined; //NaN
+let score = null; //0
+
+// let toNumber = Number(name);
+// let toNumber = Number(marks);
+// console.log(typeof toNumber);
+// console.log(toNumber);
+
+let bool = undefined;
+let converted_bool = Boolean(bool);
+console.log(typeof converted_bool);
+console.log(converted_bool);
+
+// conversion to Boolean
+// 0 => false , 1 => true
+// "" => false, "something" => true
+//100 => true
+//undefined => false
+//null => false
