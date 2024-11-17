40f09c603f95d1d293451237980c427324cc471b
mohammedshoaib2 <thismohdshoaib@gmail.com>
2024-11-17 14:25:26 +0530

    data types in JS

---
+++ 01_javascript_basics/02_datat_types.js
@@ -0,0 +1,7 @@
+let name = "Example"; //String
+let is_user_logged_in = true; //boolean
+let age = 23; //Number
+let temp = undefined; //undefined
+let date = null; //object
+let sym = Symbol(); //Symbol
+let obj = {}; //object
