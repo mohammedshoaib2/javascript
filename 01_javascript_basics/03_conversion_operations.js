a2af9b909a7528643dc351e02cc04f7f70d52ba4
mohammedshoaib2 <thismohdshoaib@gmail.com>
2024-11-17 18:04:49 +0530

    Arithmetic Operators, Pre and Postfix

--- 01_javascript_basics/03_conversion_operations.js
+++ 01_javascript_basics/03_conversion_operations.js
@@ -5,10 +5,10 @@ let is_logged_in = true; //1
 let is_eligible = undefined; //NaN
 let score = null; //0
 
-// let toNumber = Number(name);
+let toNumber = Number(name);
 // let toNumber = Number(marks);
-// console.log(typeof toNumber);
-// console.log(toNumber);
+console.log(typeof toNumber);
+console.log(toNumber);
 
 let bool = undefined;
 let converted_bool = Boolean(bool);
@@ -21,3 +21,20 @@ console.log(converted_bool);
 //100 => true
 //undefined => false
 //null => false
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 2);
+console.log(2 / 2);
+console.log(2 % 2);
+
+// prefix
+let j = 0;
+let a = ++j;
+console.log(a);
+
+// postfix
+let i = 0;
+let b = i++;
+console.log(b);
