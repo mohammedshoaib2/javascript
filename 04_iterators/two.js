2a922de1d8d2fe67e5d5f99a1f068760041f5314
mohammedshoaib2 <thismohdshoaib@gmail.com>
2024-11-21 02:11:32 +0530

    while and do while loops

---
+++ 04_iterators/two.js
@@ -0,0 +1,20 @@
+let i = 0;
+while (i <= 10) {
+  //   console.log(i);
+  //   i++;
+  i = i + 2;
+}
+
+let myArray = ["A", "B", "D", "E", "F", "G"];
+let index = 0;
+while (index < myArray.length) {
+  //   console.log(myArray[index]);
+  index++;
+}
+
+let score = 11;
+
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
